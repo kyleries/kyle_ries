b25bb0cc887f8de15a193cac9e88916b435d1e72
Kyle Ries <kyle.ries@gmail.com>
2012-08-12 18:55:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KyleRies::Application.config.secret_token = '9d472e4524f8c322c1ad11697564787fe6c4ac3e42d29c30584321d8a96e708d2bb5140a07bff7c73a12ed2a83185ddb7b599ae127239468a03404cdc0cd71e0'
